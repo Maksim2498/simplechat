335507d985144e482008de61e30dad888b28e58b
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-10-01 16:43:27 +0300

    Upgraded protocolRead

--- core/src/main/scala/ru/fominmv/simplechat/core/cli/scopt/protocolRead.scala
+++ core/src/main/scala/ru/fominmv/simplechat/core/cli/scopt/protocolRead.scala
@@ -9,8 +9,13 @@ import ru.fominmv.simplechat.core.protocol.binary.BinaryProtocol
 
 
 implicit val protocolRead: Read[Protocol] =
-    Read reads (
-        _ match
-            case "text"   => TextProtocol()
-            case "binary" => BinaryProtocol()
+    Read reads (string =>
+        val lowerString = string.toLowerCase
+
+        if "text" startsWith lowerString then
+            TextProtocol()
+        else if "binary" startsWith lowerString then
+            BinaryProtocol()
+        else
+            throw IllegalArgumentException("Bad protocol string")
     )
