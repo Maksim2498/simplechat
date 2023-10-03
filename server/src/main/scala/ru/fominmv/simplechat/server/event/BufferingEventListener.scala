23acf6548bc3c79db91cd61aae3e7cb737c2e23f
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-10-03 13:55:51 +0300

    Added BufferingEventListener

--- server/src/main/scala/ru/fominmv/simplechat/server/event/BufferingEventListener.scala
+++ server/src/main/scala/ru/fominmv/simplechat/server/event/BufferingEventListener.scala
@@ -4,21 +4,140 @@ package ru.fominmv.simplechat.server.event
 import scala.concurrent.duration.{FiniteDuration, DurationInt}
 import scala.collection.mutable.ArrayBuffer
 
+import org.apache.logging.log4j.LogManager
+
+import ru.fominmv.simplechat.core.error.ClosedException
+import ru.fominmv.simplechat.core.util.lifecycle.LifecyclePhase.*
+import ru.fominmv.simplechat.core.util.lifecycle.{LifecycleDriven, LifecyclePhase}
+import ru.fominmv.simplechat.core.util.ThreadUtil
 import ru.fominmv.simplechat.server.Client
 
 
 class BufferingEventListener(
     val bufferDuration: FiniteDuration = 10.seconds,
-)extends EventListener:
+    val autoOpen:       Boolean        = true,
+    val autoClose:      Boolean        = true,
+) extends EventListener, LifecycleDriven:
+    // External uses should be synchronized when open
     val eventListeners = ArrayBuffer[EventListener]()
 
+    override def close: Unit =
+        if !canClose then
+            return
+
+        _lifecyclePhase = CLOSING
+
+        stopPublishingThread
+        buffer.clear
+
+        _lifecyclePhase = CLOSED
+
+    override def open: Unit =
+        if !canOpen then
+            return
+
+        _lifecyclePhase = OPENING
+
+        startPublishingThread
+        waitPublishingThreadStarted
+
+        _lifecyclePhase = OPEN
+
+    override def lifecyclePhase: LifecyclePhase =
+        _lifecyclePhase
 
     override def on(event: Event): Unit =
-        ???
+        ClosedException.checkOpen(this, "Event listener is closed")
+
+        if autoOpen then
+            event match
+                case PreOpenEvent() => open
+                case _ =>
+
+        buffer synchronized {
+            buffer addOne event
+        }
+
+        if autoClose then
+            event match
+                case PostCloseEvent() => close
+                case _ =>
         
 
-    private val buffer = ArrayBuffer[Event]()
+    @volatile
+    private var _lifecyclePhase = NEW
+
+    private val buffer           = ArrayBuffer[Event]()
+    private val publishingThread = Thread(
+        () => publishingThreadBody,
+        "Publisher",
+    )
 
 
-    if bufferDuration < 0.seconds then
-        throw IllegalArgumentException("bufferDuration must be non-negative")
+    if bufferDuration <= 0.seconds then
+        throw IllegalArgumentException("bufferDuration must be positive")
+
+
+    private def startPublishingThread: Unit =
+        logger debug "Starting publishing thread..."
+        publishingThread.start
+
+    private def waitPublishingThreadStarted: Unit =
+        synchronized {
+            while
+                try
+                    wait()
+                catch
+                    case _: InterruptedException =>
+
+                publishingThread.getState == Thread.State.NEW
+            do ()
+        }
+
+    private def publishingThreadBody: Unit =
+        logger debug "Started"
+
+        synchronized {
+            notify()
+        }
+
+        while !closed do
+            try
+                logger debug "Waiting..."
+                Thread sleep bufferDuration.toMillis
+                publish
+            catch
+                case e: Exception => onAnyException(e)
+
+    private def publish: Unit =
+        logger debug "Publishing events..."
+
+        eventListeners synchronized {
+            buffer synchronized {
+                buffer foreach (event =>
+                    eventListeners foreach (_ on event)
+                )
+
+                buffer.clear
+            }
+        }
+
+        logger debug "Published"
+
+    private def stopPublishingThread: Unit =
+        ThreadUtil.stop(publishingThread, Some(logger))
+
+    private def onAnyException(exception: Exception): Unit =
+        exception match
+            case _: InterruptedException => onInterruptedException
+            case e: Exception            => onException(e)
+
+    private def onInterruptedException: Unit =
+        logger debug "Aborted: interrupted"
+        Thread.interrupted
+
+    private def onException(exception: Exception): Unit =
+        logger error exception
+
+    
+private val logger = LogManager getLogger classOf[BufferingEventListener]
